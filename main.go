91de36588cd556ad050e6a6f5347ca1f42f83bcd
Brian Ott <bott@b1-systems.de>
2024-10-07 11:25:53 +0200

    update: main to exec funcs and log errors

--- main.go
+++ main.go
@@ -1,4 +1,24 @@
 package main
 
+import (
+    "fmt"
+    "log"
+
+    "github.com/triobant/go-blog-aggregator/config"
+)
+
 func main() {
+    cfg, err := config.Read()
+    if err != nil {
+        log.Fatalf("error reading config: %v", err)
+    }
+    fmt.Printf("Read config: %+v\n", cfg)
+
+    err = cfg.SetUser("triobant")
+
+    cfg, err = config.Read()
+    if err != nil {
+        log.Fatalf("error reading config: %v", err)
+    }
+    fmt.Printf("Read config again: %+v\n", cfg)
 }
