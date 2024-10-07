9be37e760ac60ddabab983cb83d10821d8b419c6
Brian Ott <bott@b1-systems.de>
2024-10-07 11:10:44 +0200

    update: config with func to read file

--- internal/config/config.go
+++ internal/config/config.go
@@ -15,3 +15,25 @@ func (cfg *Config) SetUser(userName string) error {
     cfg.CurrentUserName = userName
     return write(*cfg)
 }
+
+func Read() (Config, error) {
+    fullPath, err := getConfigFilePath()
+    if err != nil {
+        return Config{}, err
+    }
+
+    file, err := os.Open(fullPath)
+    if err != nil {
+        return Config{}, err
+    }
+    defer file.Close()
+
+    decoder := json.NewDecoder(file)
+    cfg := Config{}
+    err = decoder.Decode(&cfg)
+    if err != nil {
+        return Config{}, err
+    }
+
+    return cfg, nil
+}
