d8f677f831e79df5e7ffbd41e9aee96158edcecf
Brian Ott <bott@b1-systems.de>
2024-10-10 19:06:53 +0200

    update: main to call new funcs

--- main.go
+++ main.go
@@ -3,22 +3,40 @@ package main
 import (
     "fmt"
     "log"
+    "os"
 
     "github.com/triobant/go-blog-aggregator/internal/config"
 )
+
+type state struct {
+    cfg *config.Config
+}
 
 func main() {
     cfg, err := config.Read()
     if err != nil {
         log.Fatalf("error reading config: %v", err)
     }
-    fmt.Printf("Read config: %+v\n", cfg)
 
-    err = cfg.SetUser("triobant")
-
-    cfg, err = config.Read()
-    if err != nil {
-        log.Fatalf("error reading config: %v", err)
+    programState := &state{
+        cfg: &cfg,
     }
-    fmt.Printf("Read config again: %+v\n", cfg)
+
+    cmds := commands{
+        registeredCommands: make(map[string]func(*state, command) error),
+    }
+    cmds.register("login", handlerLogin)
+
+    if len(os.Args) < 2 {
+        fmt.Println("Usage: cli <command> [args...]")
+        return
+    }
+
+    cmdName := os.Args[1]
+    cmdArgs := os.Args[2:]
+
+    err = cmds.run(programState, command{Name: cmdName, Args: cmdArgs})
+    if err != nil {
+        log.Fatal(err)
+    }
 }
