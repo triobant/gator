c538f74a970fc67fabdeb643858f726fe2864db6
Brian Ott <bott@b1-systems.de>
2024-10-15 10:36:32 +0200

    update: main to call register handler

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+    "database/sql"
     "fmt"
     "log"
     "os"
 
     "github.com/triobant/go-blog-aggregator/internal/config"
+    "github.com/triobant/go-blog-aggregator/internal/database"
+    _ "github.com/lib/pq"
 )
 
 type state struct {
@@ -19,13 +22,15 @@ func main() {
         log.Fatalf("error reading config: %v", err)
     }
 
-    db, err := sql.Open("postgres", dbURL)
+    db, err := sql.Open("postgres", cfg.DBURL)
     if err != nil {
-        log.Fatalf("error opening database: %v", err)
+        log.Fatalf("error connecting to database: %v", err)
     }
+    defer db.Close()
     dbQueries := database.New(db)
 
     programState := &state{
+        db:  dbQueries,
         cfg: &cfg,
     }
 
@@ -33,6 +38,7 @@ func main() {
         registeredCommands: make(map[string]func(*state, command) error),
     }
     cmds.register("login", handlerLogin)
+    cmds.register("register", handlerRegister)
 
     if len(os.Args) < 2 {
         fmt.Println("Usage: cli <command> [args...]")
