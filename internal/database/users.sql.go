f8e38ebd9b63c5182a4c70cf29a52418d000f107
Brian Ott <bott@b1-systems.de>
2024-10-15 10:35:54 +0200

    add: new generated users.sql.go from sqlc

--- internal/database/users.sql.go
+++ internal/database/users.sql.go
@@ -46,3 +46,13 @@ func (q *Queries) CreateUser(ctx context.Context, arg CreateUserParams) (User, e
 	)
 	return i, err
 }
+
+const getUser = `-- name: GetUser :one
+SELECT name FROM users WHERE name = $1
+`
+
+func (q *Queries) GetUser(ctx context.Context, name string) (string, error) {
+	row := q.db.QueryRowContext(ctx, getUser, name)
+	err := row.Scan(&name)
+	return name, err
+}
