e264157eecf338f9b1f05021d678144e873a65ab
Brian Ott <bott@b1-systems.de>
2024-10-16 13:04:19 +0200

    update: main to contain register command

--- main.go
+++ main.go
@@ -39,6 +39,7 @@ func main() {
     }
     cmds.register("login", handlerLogin)
     cmds.register("register", handlerRegister)
+    cmds.register("reset", handlerReset)
 
     if len(os.Args) < 2 {
         fmt.Println("Usage: cli <command> [args...]")
