80912ef26fbb487489e32b22e6c652108140833e
Brian Ott <bott@b1-systems.de>
2024-10-17 16:19:09 +0200

    update: register new handler

--- main.go
+++ main.go
@@ -40,6 +40,7 @@ func main() {
     cmds.register("login", handlerLogin)
     cmds.register("register", handlerRegister)
     cmds.register("reset", handlerReset)
+    cmds.register("users", handlerListUsers)
 
     if len(os.Args) < 2 {
         fmt.Println("Usage: cli <command> [args...]")
