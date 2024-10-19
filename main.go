ff49057faeb3ba1fb26496cfdccdd472d42d8ddb
Brian Ott <bott@b1-systems.de>
2024-10-19 23:33:00 +0200

    update: feed handler command registered in main

--- main.go
+++ main.go
@@ -42,6 +42,7 @@ func main() {
     cmds.register("reset", handlerReset)
     cmds.register("users", handlerListUsers)
     cmds.register("agg", handlerAgg)
+    cmds.register("addfeed", handlerAddFeed)
 
     if len(os.Args) < 2 {
         fmt.Println("Usage: cli <command> [args...]")
