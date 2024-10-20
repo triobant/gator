cad0c850694de618fbf3727e7c4638a41bda0118
Brian Ott <bott@b1-systems.de>
2024-10-20 21:56:28 +0200

    update: func to list all feeds from db with name of user that created it

--- handler_feed.go
+++ handler_feed.go
@@ -35,18 +35,42 @@ func handlerAddFeed(s *state, cmd command) error {
     }
 
     fmt.Println("Feed created successfully!")
-    printFeed(feed)
+    printFeed(feed, user)
     fmt.Println()
     fmt.Println("=====================================")
 
     return nil
 }
 
-func printFeed(feed database.Feed) {
+func handlerListFeeds(s *state, cmd command) error {
+    feeds, err := s.db.GetFeeds(context.Background())
+    if err != nil {
+        return fmt.Errorf("couldn't get feeds: %w", err)
+    }
+
+    if len(feeds) == 0 {
+        fmt.Println("No feeds found.")
+        return nil
+    }
+
+    fmt.Printf("Found %d feeds:\n", len(feeds))
+    for _, feed := range feeds {
+        user, err := s.db.GetUserById(context.Background(), feed.UserID)
+        if err != nil {
+            return fmt.Errorf("couldn't get user: %w", err)
+        }
+        printFeed(feed, user)
+        fmt.Println("=====================================")
+    }
+
+    return nil
+}
+
+func printFeed(feed database.Feed, user database.User) {
     fmt.Printf(" * ID:          %v\n", feed.ID)
     fmt.Printf(" * Created:     %v\n", feed.CreatedAt)
     fmt.Printf(" * Updated:     %v\n", feed.UpdatedAt)
     fmt.Printf(" * Name:        %v\n", feed.Name)
     fmt.Printf(" * URL:         %v\n", feed.Url)
-    fmt.Printf(" * UserID:      %v\n", feed.UserID)
+    fmt.Printf(" * User:        %v\n", user.Name)
 }
