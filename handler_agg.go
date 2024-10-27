3baabc2785ffb01d25ef9a23e993e99b229a22f3
Brian Ott <bott@b1-systems.de>
2024-10-27 21:59:02 +0100

    update: agg handler to create post in db

--- handler_agg.go
+++ handler_agg.go
@@ -2,11 +2,14 @@ package main
 
 import (
     "context"
+    "database/sql"
     "fmt"
     "log"
+    "strings"
     "time"
 
     "github.com/triobant/go-blog-aggregator/internal/database"
+    "github.com/google/uuid"
 )
 
 func handlerAgg(s *state, cmd command) error {
@@ -50,7 +53,35 @@ func scrapeFeed(db *database.Queries, feed database.Feed) {
         return
     }
     for _, item := range feedData.Channel.Item {
-        fmt.Printf("Found post: %s\n", item.Title)
+        publishedAt := sql.NullTime{}
+        if t, err := time.Parse(time.RFC1123Z, item.PubDate); err == nil {
+            publishedAt = sql.NullTime{
+                Time:   t,
+                Valid:  true,
+            }
+        }
+
+        _, err = db.CreatePost(context.Background(), database.CreatePostParams{
+            ID:             uuid.New(),
+            CreatedAt:      time.Now().UTC(),
+            UpdatedAt:      time.Now().UTC(),
+            FeedID:         feed.ID,
+            title:          item.title,
+            Description:    sql.NullString{
+                String:     item.Description,
+                Valid:      true,
+            },
+            Url:            item.Link,
+            PublishedAt:    publishedAt,
+
+        })
+        if err != nil {
+            if strings.Contains(err.Error(), "duplicate key value violates unique constraint") {
+                continue
+            }
+            log.Printf("Couldn't create post: %v", err)
+            continue
+        }
     }
     log.Printf("Feed %s collected, %v posts found", feed.Name, len(feedData.Channel.Item))
 }
