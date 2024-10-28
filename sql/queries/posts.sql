13d4d94877239a8ed773cfad4309cf7f6d5605e2
Brian Ott <bott@b1-systems.de>
2024-10-28 10:43:26 +0100

    update: posts query typo

--- sql/queries/posts.sql
+++ sql/queries/posts.sql
@@ -1,18 +1,10 @@
 -- name: CreatePost :one
-INSERT INTO feeds (id, created_at, updated_at, title, url, description, published_at, feed_id)
-VALUES (
-    $1,
-    $2,
-    $3,
-    $4,
-    $5,
-    $6,
-    $7,
-    $8
-)
+INSERT INTO posts (id, created_at, updated_at, title, url, description, published_at, feed_id)
+VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
 RETURNING *;
+--
 
--- name: GetPostsForUser :one
+-- name: GetPostsForUser :many
 SELECT posts.*, feeds.name AS feed_name FROM posts
 JOIN feed_follows ON feed_follows.feed_id = posts.feed_id
 JOIN feeds ON posts.feed_id = feeds.id
